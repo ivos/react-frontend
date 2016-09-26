894a50bdc661b67f40ccf5361665e5a6d1b3e1bc
Ivo Maixner <ivo.maixner@gmail.com>
2016-09-26 15:00:06 +0200

    Add setup react-forms-ui.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'select2/select2.css'
+import 'react-forms-ui/lib/react-forms-ui.css'
+import {initialize} from 'react-forms-ui'
+
+initialize()
+
 import App from './App'
 import './index.css'
 
