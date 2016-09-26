475a5158bba54a16a391c87313dfbb19f1443d07
Ivo Maixner <ivo.maixner@gmail.com>
2016-09-26 15:24:42 +0200

    Add react-forms-ui form.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css'
 import 'select2/select2.css'
 import 'react-forms-ui/lib/react-forms-ui.css'
 import {initialize} from 'react-forms-ui'
 
 initialize()
 
-import App from './App'
+import RegisterPage from './user/RegisterPage'
 import './index.css'
 
 ReactDOM.render(
-	<App />,
+	<RegisterPage/>,
 	document.getElementById('root')
 )
---
+++ src/user/RegisterPage.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react'
+import {Form, TextField, PasswordField, NumberField, DateField} from 'react-forms-ui'
+import {Grid, Panel} from 'react-bootstrap'
+
+const validations = {
+	myText: {
+		required: true,
+		minLength: 4,
+		maxLength: 10,
+	},
+	myNumber: {
+		required: true,
+	},
+	myDate: {},
+}
+
+class RegisterPage extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+
+	render() {
+		const fieldClasses = 'col-sm-2,col-sm-6,col-sm-4'
+		return (
+			<Grid fluid>
+				<Form state={this.state} setState={this.setState.bind(this)} validations={validations}
+				      onSubmit={this.onSubmit}>
+					<Panel header={<h3>My form</h3>}>
+						<TextField id="myText" label="My text" placeholder="Enter some text" classes={fieldClasses}/>
+						<PasswordField id="myPassword" label="My password" classes={fieldClasses}/>
+						<NumberField id="myNumber" label="My number" format="0,0.[00]" classes={fieldClasses}/>
+						<DateField id="myDate" label="My date" classes={fieldClasses}/>
+					</Panel>
+				</Form>
+			</Grid>
+		)
+	}
+
+	onSubmit() {
+		const {values} = this.state
+		console.log(values)
+	}
+}
+
+export default RegisterPage
