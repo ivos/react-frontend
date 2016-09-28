6b7a846075d066b04cd42a86fb61a5e82fe38486
Ivo Maixner <ivo.maixner@gmail.com>
2016-09-28 11:46:30 +0200

    Add register page.

---
+++ src/api.js
@@ -0,0 +1,27 @@
+const convertBackendValidationErrors = (convertFieldError, errors) => {
+	return Object.keys(errors).reduce((previous, current) => {
+		const fieldErrors = errors[current]
+		previous[current] = fieldErrors.map(
+			fieldError => convertFieldError(current, fieldError) || 'Invalid value.'
+		)
+		return previous
+	}, {})
+}
+
+const processValidationError = (form, convertFieldError, response) => {
+	if (422 === response.status) {
+		response.json()
+			.then(errors => {
+				console.log('Backend validation error', errors)
+				return errors
+			})
+			.then(errors => convertBackendValidationErrors(convertFieldError, errors))
+			.then(messages => form.setState({messages}))
+			.then(form.refs.form.focusError)
+		throw new Error('Backend validation error.')
+	} else if (response.status >= 300) {
+		console.error('Unknown server error.')
+	}
+}
+
+export {processValidationError}
--- src/user/RegisterPage.js
+++ src/user/RegisterPage.js
@@ -1,44 +1,109 @@
-import React, {Component} from 'react'
-import {Form, TextField, PasswordField, NumberField, DateField} from 'react-forms-ui'
-import {Panel} from 'react-bootstrap'
+import React from 'react'
+import {Form, FormMessages, TextField, PasswordField} from 'react-forms-ui'
+import {Panel, FormGroup, Button, HelpBlock} from 'react-bootstrap'
+import {processValidationError} from '../api'
 
 const validations = {
-	myText: {
+	username: {
 		required: true,
-		minLength: 4,
-		maxLength: 10,
+		maxLength: 100,
+		pattern: /^[a-z0-9_]*$/,
+		autoSuccess: false,
 	},
-	myNumber: {
+	email: {
 		required: true,
+		maxLength: 100,
+		pattern: /^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$/,
+		autoSuccess: false,
 	},
-	myDate: {},
+	name: {
+		required: true,
+		maxLength: 100,
+	},
+	password: {
+		required: true,
+		minLength: 6,
+		maxLength: 100,
+	},
 }
 
-class RegisterPage extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {}
-	}
+const RegisterPage = React.createClass({
+	getInitialState() {
+		return {}
+	},
 
 	render() {
 		const fieldClasses = 'col-sm-2,col-sm-6,col-sm-4'
+		const buttonsClass = 'col-sm-offset-2 col-sm-10'
 		return (
-			<Form state={this.state} setState={this.setState.bind(this)} validations={validations}
+			<Form ref="form" state={this.state} setState={this.setState.bind(this)} validations={validations}
 			      onSubmit={this.onSubmit}>
-				<Panel header={<h3>My form</h3>}>
-					<TextField id="myText" label="My text" placeholder="Enter some text" classes={fieldClasses}/>
-					<PasswordField id="myPassword" label="My password" classes={fieldClasses}/>
-					<NumberField id="myNumber" label="My number" format="0,0.[00]" classes={fieldClasses}/>
-					<DateField id="myDate" label="My date" classes={fieldClasses}/>
+				<Panel header={<h3>Register</h3>}>
+					<TextField id="username" label="Username" classes={fieldClasses}>
+						<HelpBlock>Only lower-case letters, numbers and underscores.</HelpBlock>
+					</TextField>
+					<TextField id="email" label="E-mail" classes={fieldClasses}/>
+					<TextField id="name" label="Name" classes={fieldClasses}/>
+					<PasswordField id="password" label="Password" classes={fieldClasses}>
+						<HelpBlock>At least 6 characters.</HelpBlock>
+					</PasswordField>
+
+					<FormGroup>
+						<div className={buttonsClass}>
+							<Button type="submit" bsStyle="primary">
+								<span className="fa fa-check"> </span> Register
+							</Button>
+						</div>
+					</FormGroup>
+
+					<FormMessages className={buttonsClass}/>
 				</Panel>
 			</Form>
 		)
-	}
+	},
 
 	onSubmit() {
 		const {values} = this.state
-		console.log(values)
-	}
-}
+		fetch('/api/users', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(values),
+		})
+			.then(this.handleResponse)
+			.then(() => {
+				const {username, password} = values
+				return fetch('/api/sessions', {
+					method: 'post',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({username, password}),
+				})
+			})
+			.then(response => response.json())
+			.then(session => {
+				window.localStorage.ReactFrontendSession = JSON.stringify(session)
+			})
+			.catch(
+				err => console.error(err)
+			)
+	},
+
+	handleResponse(response) {
+		processValidationError(this, this.convertFieldError, response)
+	},
+
+	convertFieldError(field, fieldErrors) {
+		if ('username' === field && 'duplicate' === fieldErrors[0]) {
+			return 'This username is already taken.'
+		}
+		if ('email' === field && 'duplicate' === fieldErrors[0]) {
+			return 'This e-mail is already registered.' +
+				' If you forgot your password, please contact the system administrator.'
+		}
+	},
+})
 
 export default RegisterPage
