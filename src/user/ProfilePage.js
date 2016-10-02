db414949168e2c38bd57c6f0d8fa19b522793175
Ivo Maixner <ivo.maixner@gmail.com>
2016-10-02 21:16:02 +0200

    Upgrade react-forms-ui.

--- src/user/ProfilePage.js
+++ src/user/ProfilePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Form, TextField, PlainField} from 'react-forms-ui'
-import {Panel, FormControl, Label, FormGroup, Button} from 'react-bootstrap'
+import {Form, TextField, CustomField} from 'react-forms-ui'
+import {Panel, Label, FormGroup, Button} from 'react-bootstrap'
 import i18n from '../i18n'
 const t = i18n.t.bind(i18n)
 import {getSession} from '../local-storage'
@@ -27,32 +27,24 @@ const ProfilePage = React.createClass({
 		return (
 			<Form state={this.state} setState={this.setState.bind(this)}>
 				<Panel header={<h3>{t('profile.title')}</h3>}>
-					<PlainField id="username" label={t('profile.username')} classes={fieldClasses} readonly>
-						<FormControl.Static>
-							<code>{values.username}</code>
-						</FormControl.Static>
-					</PlainField>
+					<CustomField id="username" label={t('profile.username')} classes={fieldClasses} readonly>
+						<code>{values.username}</code>
+					</CustomField>
 					<TextField id="name" label={t('profile.name')} classes={fieldClasses} readonly/>
-					<PlainField id="email" label={t('profile.email')} classes={fieldClasses} readonly>
-						<FormControl.Static>
-							<a href={'mailto:' + values.email}>{values.email}</a>
-						</FormControl.Static>
-					</PlainField>
-					<PlainField id="status" label={t('profile.status')} classes={fieldClasses} readonly>
-						<FormControl.Static>
-							<Label bsStyle={statusStyle(values.status)}>
-								{t('enum.user.status.' + values.status)}</Label>
-						</FormControl.Static>
-					</PlainField>
-					<PlainField id="roles" label={t('profile.roles')} classes={fieldClasses} readonly>
-						<FormControl.Static>
-							{values.roles ?
-								values.roles.split(',').map(role =>
-									<Label key={role} bsStyle="primary" className="spaced">
-										{t('enum.user.role.' + role)}</Label>
-								) : ''}
-						</FormControl.Static>
-					</PlainField>
+					<CustomField id="email" label={t('profile.email')} classes={fieldClasses} readonly>
+						<a href={'mailto:' + values.email}>{values.email}</a>
+					</CustomField>
+					<CustomField id="status" label={t('profile.status')} classes={fieldClasses} readonly>
+						<Label bsStyle={statusStyle(values.status)}>
+							{t('enum.user.status.' + values.status)}</Label>
+					</CustomField>
+					<CustomField id="roles" label={t('profile.roles')} classes={fieldClasses} readonly>
+						{values.roles ?
+							values.roles.split(',').map(role =>
+								<Label key={role} bsStyle="primary" className="spaced">
+									{t('enum.user.role.' + role)}</Label>
+							) : ''}
+					</CustomField>
 
 					<FormGroup>
 						<div className={buttonsClass}>
